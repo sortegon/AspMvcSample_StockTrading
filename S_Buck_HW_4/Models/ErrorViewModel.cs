bd62749affaeeb5da397479aee844b7515d7d2eb
Shawna <sortegon@mail.usf.edu>
2019-03-23 20:55:50 -0400

    Add project files.

---
+++ S_Buck_HW_4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S_Buck_HW_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
